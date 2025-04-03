a63f19bf6d8b69807c82df01f819473bde15eb56
ishikajais27 <ishikajais09876@gmail.com>
2025-04-03 16:21:26 +0530

    Learning interface concept in oopj

--- LAB5/SecondQ/Second.java
+++ LAB5/SecondQ/Second.java
@@ -4,25 +4,23 @@
 // class.
 // Input - mentioned in the program
 // Output - Capacity of the motor is -----
-interface Motor{
-    int capacity=100;
-    public void run();
+ interface Motor{
+   int Capacity=0;  //interface varibales are static and final so its important to initialize it
+  public void run();   //interface method does not have body
     public void consume();
-}
-class WashingMachine implements Motor{
-
-    public void run(){
-        System.out.println("The code is running!!");
+ }
+ class WashingMachine implements Motor{
+ public void run(){
+System.out.println("Runn");
     }
-    public void consume(){
-        System.out.println("Consume method!!");
+ public  void consume(){
+System.out.println("Consume");
     }
-}
-public class Second{
+ }
+ public class Second{
     public static void main(String args[]){
         WashingMachine obj = new WashingMachine();
-        System.out.println("Capacity is "+obj.capacity);
-        obj.run();
         obj.consume();
+        obj.run();
     }
-}
+ }
