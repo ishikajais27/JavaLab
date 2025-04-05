2d891b41344321c61767afab45bb447283907ea6
ishikajais27 <ishikajais09876@gmail.com>
2025-04-05 13:09:17 +0530

    Revising Exception Topic

--- LAB6/Fourth.java
+++ LAB6/Fourth.java
@@ -1,59 +1,53 @@
-import java.util.Scanner;
+// Aim of the Program: Write a program to create user defined exceptions called
+// HrsException, MinException and SecException. Create a class Time which contains data
+// members hours, minutes, seconds and a method to take a time from user which throws
+// the user defined exceptions if hours (&gt;24 &amp;&lt;0),minutes(&gt;60 &amp;&lt;0),seconds(&gt;60 &amp;&lt;0).
 
-class HrsException extends Exception {
-    public HrsException(String message) {
-        super(message);
-    }
+import java.util.*;
+class HrsException extends Exception{
+ public HrsException(String m){
+    super(m);
+ }
 }
-class MinException extends Exception {
-    public MinException(String message) {
-        super(message);
-    }
+class MinException extends Exception{
+ public MinException(String m){
+    super(m);
+ }
 }
-class SecException extends Exception {
-    public SecException(String message) {
-        super(message);
-    }
+class SecException extends Exception{
+ public SecException(String m){
+    super(m);
+ }
 }
-public class Fourth {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter hours:");
-        int h = scanner.nextInt();
-        System.out.println("Enter minutes:");
-        int m = scanner.nextInt();
-        System.out.println("Enter seconds:");
-        int s = scanner.nextInt();
-
+class Time{
+    int hr,min,sec;
+    Time(int x,int y,int z){
         try {
-            Time t = new Time();
-            t.setTime(h, m, s);
-            System.out.println("Correct Time -> " + t.getTime());
-        } catch (HrsException | MinException | SecException e) {
-            System.out.println("Exception occurred: " + e.getMessage());
+            this.hr = x;
+        this.min = y;
+        this.sec = z;
+        if(hr>24 || hr<0){
+            throw new HrsException("Invalid hour input");
+        }
+        else  if(min>60 || min<0){
+            throw new HrsException("Invalid min input");
+        }
+       else  if(min>24 || sec<0){
+            throw new HrsException("Invalid sec input");
+        }
+       System.out.println("Time - "+hr+":"+min+":"+sec);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
-
-class Time {
-    int hours, minutes, seconds;
-
-    public void setTime(int h, int m, int s) throws HrsException, MinException, SecException {
-        if (h < 0 || h > 24) {
-            throw new HrsException("Invalid hours");
-        }
-        if (m < 0 || m > 60) {
-            throw new MinException("Invalid minutes");
-        }
-        if (s < 0 || s > 60) {
-            throw new SecException("Invalid seconds");
-        }
-        hours = h;
-        minutes = m;
-        seconds = s;
-    }
-
-    public String getTime() {
-        return hours + ":" + minutes + ":" + seconds;
+public class Fourth{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter hour,min and sec- ");
+        int hr = in.nextInt();
+        int min = in.nextInt();
+        int sec = in.nextInt();
+        Time obj = new Time(hr,min,sec);
     }
 }
