9a79a2daad30024d46eb082cdd251ed3c12ee3e6
ishikajais27 <ishikajais09876@gmail.com>
2025-04-05 12:48:49 +0530

    Revising Exception Topic

--- LAB6/Third.java
+++ LAB6/Third.java
@@ -1,30 +1,34 @@
-import java.util.Scanner;
-
-class NegativeNumberException extends Exception {
-    public NegativeNumberException(String message) {
+// Aim of the program- Write a Java class which has a method called ProcessInput(). This
+// method checks the number entered by the user. If the entered number is negative then
+// throw an user defined exception called NegativeNumberException, otherwise it displays
+// the double value of the entered number.
+// Input: Enter a number 4
+// Output: Double value: 8
+// Input: Enter a number -4
+// Output: Caught the exception
+// Exception occurred: NegativeNumberException: number should be
+// positive
+import java.util.*;
+//Genral synatx to build user defined exception
+class NegativeNumberException extends  Exception{
+          public NegativeNumberException(String message) {
         super(message);
-    }
+          } 
 }
 
-public class Third {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int num = scanner.nextInt();
-
+public class Third{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number - ");
+        int n = in.nextInt();
         try {
-            ProcessInput(num);
-        } catch (NegativeNumberException e) {
-            System.out.println("Caught the exception");
-            System.out.println("Exception occurred: " + e);
+            if(n<0){
+                throw new NegativeNumberException("You entered negative number!!!");
+            }
+              System.out.println((n*n));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-    }
 
-    public static void ProcessInput(int num) throws NegativeNumberException {
-        if (num < 0) {
-            throw new NegativeNumberException("number should be positive");
-        } else {
-            System.out.println("Double value: " + (num * 2));
-        }
     }
 }
