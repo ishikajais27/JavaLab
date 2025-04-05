8b0bb67e138f5f2df0d1b93b953669f42a4359ce
ishikajais27 <ishikajais09876@gmail.com>
2025-04-05 17:46:30 +0530

    Update

--- ENDSEM/Second.java
+++ ENDSEM/Second.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 public class Second {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st string");
         String a = sc.nextLine();
+        System.out.println("Enter 1st string");
         String b = sc.nextLine();
-        int len = Math.min(a.length(), b.length());
-        int res = 0;
+        int count = 0;
 
-        for (int i = 0; i < len; i++) {
+        if(a.length()!=b.length()){
+            System.out.println("Strings are not same!!!");
+        }
+        else{
+            for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) {
-                res = a.charAt(i) - b.charAt(i);
+                System.out.println("Given strings are not equal!!");
                 break;
+            } else {
+                count++;
             }
         }
-
-        if (res == 0)
-            res = a.length() - b.length();
-
-        if (res == 0)
-            System.out.println("Equal");
-        else if (res < 0)
-            System.out.println("First is smaller");
-        else
-            System.out.println("First is larger");
+        if (count == (a.length())) {
+            System.out.println("Strings are same");
+        }
+        }
     }
 }
